7d38ae3a192c28935ecaf91fb5763d04913504dc
Leandro <githhb@mittere.net>
2017-03-15 01:28:32 +0100

    lab 3 in progress

--- mod-3/lab/begin/todos/src/app/app.module.ts
+++ mod-3/lab/begin/todos/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { TodoListComponent } from './todo-list/todo-list.component';
+import { TodoItemComponent } from './todo-item/todo-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TodoListComponent
+    TodoListComponent,
+    TodoItemComponent
   ],
   imports: [
     BrowserModule,
--- mod-3/lab/begin/todos/src/app/todo-list/todo-list.component.ts
+++ mod-3/lab/begin/todos/src/app/todo-list/todo-list.component.ts
@@ -8,14 +8,35 @@ import { Component, OnInit } from '@angular/core';
 export class TodoListComponent implements OnInit {
 
   todos: any[] = [];
+  newTodo: string = "nieuwe entry";
 
   constructor() { }
 
   ngOnInit() {
     this.todos = [
-      {title: "brood", done: true},
+      {title: "brood", done: false},
+      {title: "melk", done: true},
+      {title: "vlees", done: false},
+      {title: "pindakaas", done: true},
       {title: "kaas", done: false}
     ]
   }
 
+  addTodo () {
+    this.todos.push({
+      title: this.newTodo,
+      done: false
+    });
+    this.newTodo = '';
+  }
+
+  clearDone () {
+    for (let t in this.todos) {
+      if (this.todos[t].done) this.todos.splice(+t, 1);
+    }
+  }
+
+  noDoneElementsLeft(): boolean {
+    return this.todos.every(item => !item.done);
+  }
 }
